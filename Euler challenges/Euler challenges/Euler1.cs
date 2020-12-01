d1139f690fb0ae92c4ec3fec9f5d2f87f84c468b
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2020-12-01 17:26:15 +0100

    no message

---
+++ Euler challenges/Euler challenges/Euler1.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Diagnostics;
+
+namespace Euler_challenges
+{
+    class Euler1
+    {
+        public static void Start()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            int suma = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    suma += i;
+                }
+            }
+            Console.WriteLine(suma);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.ReadLine();
+        }
+
+    }
+
+}
